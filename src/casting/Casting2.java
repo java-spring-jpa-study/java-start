58a7b74a8931f83d13eb4f8a2cca87da031638d0
Jisu Eun <zkxktmxmfhvl@gmail.com>
2025-04-02 17:00:08 +0900

    Casting

--- src/casting/Casting2.java
+++ src/casting/Casting2.java
@@ -2,6 +2,11 @@ package casting;
 
 public class Casting2 {
     public static void main(String[] args) {
-        double doubleValue;
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+        //intValue = doubleValue; //컴파일 오류 발생
+        intValue = (int) doubleValue; //형변환
+        System.out.println(intValue);
     }
 }
