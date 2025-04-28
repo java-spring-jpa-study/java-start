23d7057a44f66ab08755c165497c8ab74bd668e4
Jisu Eun <zkxktmxmfhvl@gmail.com>
2025-04-28 14:34:13 +0900

    method

---
+++ src/overloading/ex/MethodEx2Ref.java
@@ -0,0 +1,17 @@
+package overloading.ex;
+
+public class MethodEx2Ref {
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+
+        printMessage(message, 3);
+        printMessage(message, 5);
+        printMessage(message, 7);
+    }
+
+    public static void printMessage(String message, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(message);
+        }
+    }
+}
